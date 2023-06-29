bf0f0f2094941f4914819108a604d019d1ec1793
Ignacio Grillo Caimary <igrilloc@pop-os.localdomain>
2023-06-29 18:36:06 -0300

    completed app

---
+++ src/hooks/useCatFact.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { getRandomFact } from "../services/facts";
+
+
+export function useCatFact() {
+
+  const [fact, setFact] = useState();
+  
+  const refreshFact = () => {
+    getRandomFact().then((newFact) => setFact(newFact));
+  }
+  
+  // Para recuperar la cita al cargar la página
+  useEffect(refreshFact, []);
+  
+  return { fact, refreshFact}
+  
+}
---
+++ src/hooks/useCatImage.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com"
+
+
+export function useCatImage({ fact }) {
+
+  const [imageUrl, setImageUrl] = useState();
+  
+  // Para recuerar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+  
+    if(!fact) return
+  
+    const threeFirstWords = fact.split(' ', 3).join(' ');
+    // console.log(threeFirstWords);
+  
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then((response) => response.json())
+      .then((data) => {
+        const { url } = data
+        setImageUrl(url)
+      })
+  
+  }, [fact]);
+  
+  return { imageUrl: `${CAT_PREFIX_IMAGE_URL}${imageUrl}` }
+  
+}
